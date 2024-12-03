f857bc8f29cada841082bea99c4cd9dcdd03c128
embunazzahra <dhauembunazzahra@gmail.com>
2024-12-03 14:38:35 +0700

    Initial Commit

---
+++ MenuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuApp
+//
+//  Created by Dhau Embun Azzahra on 03/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MenuAppTests/MenuAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppTests.swift
+//  MenuAppTests
+//
+//  Created by Dhau Embun Azzahra on 03/12/24.
+//
+
+import Testing
+@testable import MenuApp
+
+struct MenuAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
